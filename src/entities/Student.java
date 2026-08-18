f12ec8a4eb6689241a6ee2bfcf5f09c4128f022f
mohammed said <mohed.said77@gmail.com>
2026-08-18 14:31:23 +0400

    Constructor

--- src/entities/Student.java
+++ src/entities/Student.java
@@ -7,7 +7,11 @@ public class Student extends Person {
     private double feeBalance;
     private List<String> subjects;
 
-
+    public Student(String id, String firstName, String lastName,
+                   String dateOfBirth, String gender,
+                   String phoneNumber, String email, String address,
+                   String gradeLevel, String enrollmentDate,
+                   double feeBalance) {
 
 
 
