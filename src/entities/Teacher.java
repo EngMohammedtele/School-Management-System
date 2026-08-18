63f3ff6b4a6f2db2e8016ede40c6cfba8e193431
mohammed said <mohed.said77@gmail.com>
2026-08-18 14:52:34 +0400

    Constructor

--- src/entities/Teacher.java
+++ src/entities/Teacher.java
@@ -11,4 +11,9 @@ public class Teacher extends Person {
     private int experienceYears;
     private double salary;
     private List<String> classesTaught;
+    public Teacher(String id, String firstName, String lastName,
+                   String dateOfBirth, String gender,
+                   String phoneNumber, String email, String address,
+                   String subject, int experienceYears,
+                   double salary) {
 }
