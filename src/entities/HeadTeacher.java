a81f2118653bb9fd3f99db3cdc5e26e6900be18c
mohammed said <mohed.said77@gmail.com>
2026-08-18 15:08:21 +0400

    Constructor

--- src/entities/HeadTeacher.java
+++ src/entities/HeadTeacher.java
@@ -4,4 +4,12 @@ public class HeadTeacher extends  Person{
 
     private String department;
     private int teachersManaged;
-}
+
+    public HeadTeacher(String id, String firstName, String lastName,
+                       String dateOfBirth, String gender,
+                       String phoneNumber, String email, String address,
+                       String subject, int experienceYears,
+                       double salary, String department,
+                       int teachersManaged) {
+
+    }
